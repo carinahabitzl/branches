dc160db5c8c88caf86d8ba17ce64a5145d53f188
carinahabitzl <habitzlcarina@mgail.com>
2021-11-21 10:01:25 +0100

    Mathe Klasse erstellt

---
+++ src/main/java/Mathe.java
@@ -0,0 +1,14 @@
+public class Mathe {
+
+    private int a;
+    private int b;
+
+    public Mathe(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int add(int a, int b){
+        return a +b;
+    }
+}
